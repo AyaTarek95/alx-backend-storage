f90ba8bd9dfa95195e7df7440caed174fed69ae3
AyaTarek95 <aya.tarek213@icloud.com>
2024-05-01 21:14:35 +0300

    task1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -7,13 +7,33 @@ from typing import Union
 
 class Cache:
     """ a Cache class"""
-    def __init__(self)-> None:
+    def __init__(self) -> None:
         """ init of the class"""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[str, bytes, int, float])-> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """store methode"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Any] = None) -> Any:
+        """ get method"""
+        value = self._redis.get(key)
+        if fn:
+            if fn == int:
+                return self.get_int(value)
+            elif fn == str:
+                return self.get_str(value)
+            else:
+                return fn(value)
+        return value
+
+    def get_str(value: bytes) -> str:
+        """ get str methode"""
+        return str(value)
+
+    def get_int(value: bytes) -> int:
+        """ get int methode"""
+        return int(value)
