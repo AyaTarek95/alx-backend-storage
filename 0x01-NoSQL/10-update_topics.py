62547812f5b5db17343cefa935c1e60c1cd201f4
AyaTarek95 <aya.tarek213@icloud.com>
2024-05-01 22:13:54 +0300

    fix10

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-"""changes all topics of a school"""
+"""update_topics function"""
 
 
 def update_topics(mongo_collection, name, topics):
     """changes all topics of a school"""
-    mongo_collection.update_many({"name": name},{$"set": {"topics": topics}})
+    mongo_collection.update_many({"name": name},{"$set": {"topics": topics}})
